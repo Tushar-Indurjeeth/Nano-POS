d30a2029617db85e3caddb6da2e290592d422d42
Snow <tusharindurjeeth@protonmail.com>
2025-11-18 02:14:49 +0200

    Added dotenv so env variables are imported correctly

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,22 @@
-import { Pool } from "pg";
+import { Pool, PoolConfig } from "pg";
+import * as dotenv from "dotenv";
 
-const pool = new Pool({
-  user: process.env.DB_USER || "nano_pos",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_DATABASE || "nano_pos",
-  password: process.env.DB_PASSWORD || "nano_pos",
-  port: Number(process.env.DB_PORT) || 5432,
-});
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const config: PoolConfig = isProduction
+  ? {
+      connectionString: process.env.DATABASE_URL,
+    }
+  : {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+    };
+
+const pool = new Pool(config);
 
 export default pool;
